e6676adf4fe6bd8e076faa252c38529a95511c39
Jin Dai <jindai1783@gmail.com>
2014-12-18 14:00:21 +0000

    Finished

--- lib/fleet.rb
+++ lib/fleet.rb
@@ -4,10 +4,10 @@ class Fleet
   def initialize
     @fleet = {
       "aircraft carrier" => 5,
-      # "battleship" => 4,
-      # "submarine" => 3,
-      # "destroyer" => 3,
-      # "patrol boat" => 2,
+      "battleship" => 4,
+      "submarine" => 3,
+      "destroyer" => 3,
+      "patrol boat" => 2,
     }
   end
 
